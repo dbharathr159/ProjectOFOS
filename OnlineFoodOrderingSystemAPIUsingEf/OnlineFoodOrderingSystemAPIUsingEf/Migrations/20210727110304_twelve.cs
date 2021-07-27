91d9549a586b6d391e87bef76ffc96ea73b84a03
dbharathr159 <dbharathr159@gmail.com>
2021-07-27 18:27:24 +0530

    CommitProjectCode

---
+++ OnlineFoodOrderingSystemAPIUsingEf/OnlineFoodOrderingSystemAPIUsingEf/Migrations/20210727110304_twelve.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineFoodOrderingSystemAPIUsingEf.Migrations
+{
+    public partial class twelve : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Menu",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Menu",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
---
+++ OnlineFoodOrderingSystemAPIUsingEf/OnlineFoodOrderingSystemAPIUsingEf/Migrations/20210727123851_fourteen.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineFoodOrderingSystemAPIUsingEf.Migrations
+{
+    public partial class fourteen : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalAmount",
+                table: "Orders");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "Decimal",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
